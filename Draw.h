b08356678127467d8f4afc8a158d0f3d540150a1
Ryan <civmaster64@gmail.com>
2019-11-16 17:35:10 -0600

    Delete Draw.h

--- Draw.h
+++
@@ -1,8 +0,0 @@
-#include<allegro5/allegro.h>
-#include<allegro5/allegro_image.h>
-#include"Pictures.h"
-
-void RederGraphics();
-void PlayerShootingAnimation();
-
-ALLEGRO_BITMAP* Place(ALLEGRO_BITMAP* image);
